102f00282ecec1f2ed3b4768c0a3c8982eb93d5d
gmbyapa <gmbyapa@gmail.com>
2019-10-20 20:28:32 +0530

    go 1.13 error support added

--- errors.go
+++ errors.go
@@ -1,53 +1,28 @@
 package errors
 
 import (
-	"errors"
 	"fmt"
 	"runtime"
 )
 
-type Error struct {
-	previous error
-	current  error
+func New(message string) error {
+	return fmt.Errorf("%s on (%s)", message, filePath())
 }
 
-func (e *Error) Error() string {
-
-	if e.previous != nil {
-		//return fmt.Sprintf("%s due to\n%s", e.current.Error(),  e.previous.Error())
-		return fmt.Sprintf("%s due to %s", e.current.Error(), e.previous.Error())
-	}
-
-	return e.current.Error()
+func Errorf(format string, a ...interface{}) error {
+	return fmt.Errorf(format+" on (%s)", append(a, filePath())...)
 }
 
-func (e Error) Errors() []error {
-	return nil
-}
-
-func New(prefix string, message interface{}) error {
-	return &Error{
-		current: errors.New(fmt.Sprintf(`(%s) (%+v%s)`, prefix, message, filePath())),
-	}
-}
-
-func WithPrevious(previous error, prefix string, message interface{}) error {
-
-	current := errors.New(
-		fmt.Sprintf(`(%s) (%+v%s)`, prefix, message, filePath()))
-
-	return &Error{
-		previous: previous,
-		current:  current,
-	}
+func WithPrevious(previous error, message string) error {
+	return fmt.Errorf("%s on (%s) due to %w", message, filePath(), previous)
 }
 
 func filePath() string {
-	_, f, l, ok := runtime.Caller(2)
-	if !ok {
-		f = `<Unknown>`
-		l = 1
+	pc, _, _, ok := runtime.Caller(2)
+	fn := `unknown`
+	if ok {
+		fn = runtime.FuncForPC(pc).Name()
 	}
 
-	return fmt.Sprintf(` on %s:%d`, f, l)
+	return fmt.Sprintf(`%s`, fn)
 }
